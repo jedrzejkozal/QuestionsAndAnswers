6cd8400dc94c4365ba9997496a9ba43813e33373
jedrzejkozal <jedrzej.kozal@gmail.com>
2019-10-14 21:08:22 +0200

    placeholders for terms of condition and privacy policy

--- QaA/ask/urls.py
+++ QaA/ask/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, TemplateView
 
 from . import views
 
@@ -8,5 +8,9 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('login', LoginView.as_view(template_name='ask/login.html'), name='login'),
     path('signup', views.SignUpView.as_view(), name='signup'),
-    path('user', views.user_profile, name='user')
+    path('user', views.user_profile, name='user'),
+    path('terms', TemplateView.as_view(
+        template_name='ask/terms.html'), name='terms'),
+    path('privacypolicy', TemplateView.as_view(
+        template_name='ask/privacypolicy.html'), name='privacypolicy')
 ]
